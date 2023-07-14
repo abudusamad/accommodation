dc5370ef48d5aee9bb7ccf8c52350602fae902c5
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-07-14 00:05:56 +0000

    Finished with the booking of the hostel

--- src/pages/ImageGallery.jsx
+++ src/pages/ImageGallery.jsx
@@ -4,33 +4,20 @@ import {
 	CardContent,
 	Container,
 	Grid,
-	ListItem,
 	Typography,
 } from "@mui/material";
 import React, { useState } from "react";
 import { Toaster } from "react-hot-toast";
-import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
+
 import { BookingModal } from "../components/BookingModal";
-import { getHotelBySlug } from "../components/api/request";
 import { bookings } from "../data/dummy";
 
 export const ImageGallery = () => {
 	const [open, setOpen] = useState(false);
 
-	const params = useParams();
-	const { slug } = params;
-
-	const fetchHoteInfo = async () => {
-		const { data } = await getHotelBySlug(slug);
-		return data;
-	};
-
 	const handleOpen = () => setOpen(true);
 
 	const handleClose = () => setOpen(false);
-
-	const { data } = useQuery("hotel-info", fetchHoteInfo);
 
 	return (
 		<>
@@ -55,24 +42,7 @@ export const ImageGallery = () => {
 					marginTop: 2,
 				}}
 			>
-				<Typography fontSize={22} sx={{ lineHeight: 1.9, marginBottom: 3 }}>
-					{data?.name}
-				</Typography>
-				<ImageGallery images={data?.images} />
-
-				<Box
-					sx={{ display: "flex", marginTop: 2, gap: "0 12px", color: "gray" }}
-				>
-					{data?.rooms.map((room) => (
-						<Typography key={room.id} variant="caption">
-							{room.content}
-						</Typography>
-					))}
-				</Box>
-				<Typography variant="subtitle1" sx={{ marginTop: 2 }}>
-					{data?.aboutThePlace}
-				</Typography>
-				<Box sx={{ marginTop: 2 }}>
+				<Box sx={{ marginTop: 2, display:"flex", justifyContent:"space-between" }}>
 					<Typography variant="h5">What this place offers!!</Typography>
 					<Box
 						sx={{
@@ -81,30 +51,24 @@ export const ImageGallery = () => {
 							alignItems: "center",
 						}}
 					>
-						<Box sx={{ flex: 1 }}>
-							{data?.features.map((feature) => (
-								<ListItem key={feature.id}>{feature.text}</ListItem>
-							))}
-						</Box>
-
 						<CardContent>
 							<Button onClick={handleOpen} variant="outlined">
 								Reserve
 							</Button>
 						</CardContent>
 					</Box>
+					<BookingModal open={open} handleClose={handleClose} />
+					<Toaster
+						position="top-right"
+						toastOptions={{
+							duration: 1500,
+							style: {
+								fontSize: 14,
+							},
+						}}
+					/>
 				</Box>
 			</Container>
-			<BookingModal hotelInfo={data} open={open} handleClose={handleClose} />
-			<Toaster
-				position="top-right"
-				toastOptions={{
-					duration: 1500,
-					style: {
-						fontSize: 14,
-					},
-				}}
-			/>
 		</>
 	);
 };
