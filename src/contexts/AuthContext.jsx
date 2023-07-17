8d0f081fe6614c567da8bd41a7bad1583dfd53ff
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-07-17 00:56:57 +0000

    working with the api of the pages

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,9 +1,10 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useReducer } from "react";
 import { auth } from "../lib/firebase";
 export const AuthContext = createContext({});
 
 export const AuthContextProvider = ({ children}) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -13,10 +14,67 @@ export const AuthContextProvider = ({ children}) => {
     });
     return () => unsubscribe();
   }, []);
+   useEffect(() => {
+			localStorage.setItem("user", JSON.stringify(state.user));
+		}, [state.user]);
+
 
   return (
-    <AuthContext.Provider value={{ currentUser }}>
+    <AuthContext.Provider value={ { currentUseruser: state.user,
+        loading: state.loading,
+        error: state.error,
+        dispatch, } }>
+        
+  
       {children}
     </AuthContext.Provider>
   );
 };
+
+  // if there is user                         // if no user present
+
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  loading: false,
+  error: null,
+};
+
+
+
+const AuthReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null, 
+        loading: true,
+        error: null,
+      };
+    case "LOGIN_SUCCESS": 
+      return {       
+        user: action.payload,
+        loading: false,
+        error: null,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: null,
+        loading: false,
+        error: action.payload,
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+
+ 
+  //saving user in local storage to user will not get logout automatically with page refresh.
+
+
+// https://stackoverflow.com/questions/69917833/cannot-access-object-values-from-usecontext-using-usereducer-and-usecontext-for
