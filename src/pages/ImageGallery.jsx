b29ffc3ff3dd8b38a00b148f88eea0c80fce888d
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-07-17 23:05:18 +0000

    working with booking

--- src/pages/ImageGallery.jsx
+++ src/pages/ImageGallery.jsx
@@ -28,7 +28,7 @@ export const ImageGallery = ({ images }) => {
 
 	const { data } = useQuery("hotel-info", fetchHoteInfo);
 	return (
-		<Box>
+		<Box width="98%">
 			<Grid container spacing={0}>
 				{bookings?.map((Booking, index) => (
 					<Grid key={index.id} md={4} xs={12}>
@@ -37,19 +37,12 @@ export const ImageGallery = ({ images }) => {
 							alt={`Hostel ${Booking + 1}`}
 							style={{
 								width: "auto",
-								height: "100%",
+								height: "98%",
 							}}
-							className="p-3 rounded after:bg-black after:absolute top-0 left-0 after:opacity-0 after: translate-x-7 hover:opacity-100"
+							className="p-3"
+							sx={{backgroundColor:colors.grey[400]}}
 						/>
-						<Box>
-							<Typography
-								variant="h2"
-								sx={{ zIndex: "1000" }}
-								className="opacity-0 translate-x-3 hover:opacity-100 text-6xl text-white flex  mx-96 px-52 mt-52"
-							>
-								{Booking.text}
-							</Typography>
-						</Box>
+						
 					</Grid>
 				))}
 			</Grid>
@@ -62,11 +55,6 @@ export const ImageGallery = ({ images }) => {
 					}}
 				>
 					<Typography variant="h5">What this place offers!!</Typography>
-					{/* <Box sx={{ flex: 1 }}>
-						{bookings.features.map((feature) => (
-							<ListItem key={feature.id}>{feature.text}</ListItem>
-						))}
-					</Box> */}
 
 					<CardContent>
 						<Button
