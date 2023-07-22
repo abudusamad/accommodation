370693bd8096f8456d49a4186d5d56d9ba4cbd0e
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-07-22 00:07:09 +0000

    Finished with the filtering with the hostel and homestel

--- src/pages/Recommended/Recommended.js
+++ src/pages/Recommended/Recommended.js
@@ -1,14 +1,27 @@
-
-import "./Recommended.css";
+import { Box, useTheme} from "@mui/material";
 import Button from "../../components/Button";
 import { tokens } from "../../theme";
-import { Box, useTheme } from "@mui/material";
-
+import "./Recommended.css";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import Filter from "../filter/Filter";
 
 const Recommended = ({ handleClick }) => {
-  const theme = useTheme();
-	const colors = tokens(theme.palette.mode);
-  return (
+	const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [open, setOpen] = useState(false);
+
+
+	const fetchHoteInfo = async () => {
+		return data;
+	};
+
+	const handleOpen = () => setOpen(true);
+
+	const handleClose = () => setOpen(false);
+
+	const { data } = useQuery("hotel-info", fetchHoteInfo);
+	return (
 		<>
 			<div>
 				<h2 className="recommended-title">Recommended</h2>
@@ -24,8 +37,15 @@ const Recommended = ({ handleClick }) => {
 						value="Homestel"
 						title="Homestel"
 					/>
-					<Button onClickHandler={handleClick} value="Filter" title="Filter" />
+					<Button
+						onClick={handleOpen}
+						variant="outlined"
+						className="hover:bg-white"
+						sx={{ backgroundColor: "white" }}
+						title="Filter"
+					/>
 				</Box>
+				<Filter hotelInfo={data} open={open} handleClose={handleClose} />
 			</div>
 		</>
 	);
